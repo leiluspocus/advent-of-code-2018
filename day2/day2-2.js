a9b501dbe06f191eaca8b9d3ab16a785f4fb70d6
Laila Atrmouh <laila.atrmouh@gmail.com>
2018-12-09 15:37:03 +0100

    puzzle 2 - day 2

---
+++ day2/day2-2.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+const countDiffCharacters = (str1, str2) => { 
+    let differentChars = 0;
+    for ( var i=0 ; i < str1.length ; i++) {
+        if ( str1.charAt(i) !== str2.charAt(i) ) {
+            differentChars++;
+        }
+    }
+    return differentChars;
+}
+
+const extractCommonChars = (str1, str2) => {
+    let a = '';
+    for ( var i=0 ; i < str1.length ; i++) {
+        if ( str1.charAt(i) === str2.charAt(i) ) {
+            a = a + str1.charAt(i);
+        }
+    }
+    return a;   
+}
+
+const checksum = () => {
+    fs.readFile('input', (err, data) => {
+        arr = data.toString().split('\n');
+        var str = '';
+        min = countDiffCharacters(arr[0], arr[1]);
+        for ( var i=0; i<arr.length; i++) {
+            for ( var j=i+1; j < arr.length; j++) {
+                const diff = countDiffCharacters(arr[i], arr[j]);
+                if ( diff < min ) { 
+                    str = extractCommonChars(arr[i], arr[j]); 
+                    min = diff;
+                }
+            }
+        }
+        console.log("Common chars: " + str);
+    });
+}
+
+checksum();
